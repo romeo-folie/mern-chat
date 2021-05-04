90774c98124a41952c0abac63984e824b5463310
Romeo Folie <romeofolie1@gmail.com>
2021-05-04 02:55:12 +0000

    resized chat bubbles in message wrapper

--- client/src/components/chat/chat.styles.jsx
+++ client/src/components/chat/chat.styles.jsx
@@ -1,5 +1,5 @@
-import tw from 'tailwind-styled-components'
-import { InfoContainer, UserSubTitle } from '../user-info/user-info.styles'
+import tw from "tailwind-styled-components";
+import {InfoContainer, UserSubTitle} from "../user-info/user-info.styles";
 
 export const ChatContainer = tw.div`
   flex
@@ -8,24 +8,25 @@ export const ChatContainer = tw.div`
   w-3/4
   pb-4
   justify-between
-`
+`;
 
 export const MessageWrapper = tw.div`
   p-5
-  block
+  flex
+  flex-col
   h-full
   w-full
   overflow-y-auto
   overscroll-contain
-`
+`;
 
 export const ChatUserInfoContainer = tw(InfoContainer)`
   bg-transparent
-`
+`;
 
 export const SubTitle = tw(UserSubTitle)`
   text-ash
-`
+`;
 
 export const ChatHeader = tw.div`
   h-20
@@ -34,7 +35,7 @@ export const ChatHeader = tw.div`
   bg-white
   items-center
   px-3
-`
+`;
 
 export const BlockButton = tw.button`
   border
@@ -49,7 +50,7 @@ export const BlockButton = tw.button`
   hover:text-white
   hover:bg-red-600
   focus:outline-none
-`
+`;
 
 export const UnblockButton = tw.button`
   border
@@ -64,7 +65,7 @@ export const UnblockButton = tw.button`
   border-green-600
   text-green-600
   hover:bg-green-600
-`
+`;
 
 export const ChatFooter = tw.div`
   flex
@@ -72,7 +73,7 @@ export const ChatFooter = tw.div`
   w-full
   justify-around
   items-center
-`
+`;
 
 export const ChatInput = tw.input`
   w-4/5
@@ -84,7 +85,7 @@ export const ChatInput = tw.input`
   border-teal
   pl-3
   focus:outline-none
-`
+`;
 
 export const SendButton = tw.button`
   bg-teal
@@ -97,28 +98,26 @@ export const SendButton = tw.button`
   focus:outline-none
   disabled:opacity-50
   disabled:cursor-text
-`  
+`;
 
-export const SentMessage = tw.div`
+export const SentMessage = tw.span`
   ml-auto
   p-3
   rounded-lg
   bg-teal
-  max-w-1/4
+  max-w-1/2
   text-white
   my-1
   text-sm	
-`
+`;
 
-export const ReceivedMessage = tw.div`
+export const ReceivedMessage = tw.span`
   mr-auto
   p-3
   rounded-lg
   bg-dark-blue
   text-white
   my-1
-  max-w-1/4
+  max-w-1/2
   text-sm
-`
-
-
+`;
