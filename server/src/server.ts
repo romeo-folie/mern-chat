686e02c5779362c536bd95eccf346b4db662fe2d
Romeo Folie <romeofolie1@gmail.com>
2021-05-09 13:22:10 +0000

    removed client .env from git cache

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import app from "./app";
-import { Server } from "socket.io";
+import {Server} from "socket.io";
 import http from "http";
-import { connectUser, disconnectUser } from "./utils/users";
-import Chat, { IChat } from "./models/chat";
+import {connectUser, disconnectUser} from "./utils/users";
+import Chat, {IChat} from "./models/chat";
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -16,79 +16,82 @@ const port = 5000;
 
 io.on("connection", (socket) => {
   socket.on("new user", (userObj, cb) => {
-    const users = connectUser({ id: socket.id, ...userObj });
+    const users = connectUser({id: socket.id, ...userObj});
     io.emit("users", users);
   });
 
   // add a handler for when a message is sent
   // it should save the message in mongodb
-  socket.on("out message", async (data, cb): Promise<void> => {
-    const { message, sender, recepient } = data;
+  socket.on(
+    "out message",
+    async (data, cb): Promise<void> => {
+      const {message, sender, recepient} = data;
 
-    const chat: IChat = new Chat({
-      senderEmail: sender.email,
-      recepientEmail: recepient.email,
-      message: message.trim(),
-      date: new Date(),
-    });
-
-    const savedMessage = await chat.save();
-
-    socket
-      .to(recepient.id)
-      .emit("in message", {
-        sender,
-        message: { text: savedMessage.message, id: savedMessage._id },
+      const chat: IChat = new Chat({
+        senderEmail: sender.email,
+        recepientEmail: recepient.email,
+        message: message.trim(),
+        date: new Date(),
       });
-    cb({
-      sender,
-      message: { text: savedMessage.message, id: savedMessage._id },
-    });
-  });
+
+      const savedMessage = await chat.save();
+
+      socket.to(recepient.id).emit("in message", {
+        sender,
+        message: {text: savedMessage.message, id: savedMessage._id},
+      });
+      cb({
+        sender,
+        message: {text: savedMessage.message, id: savedMessage._id},
+      });
+    }
+  );
 
   // another handler that gets all the old messages
-  socket.on("load conversation", async ({ sender, recepient }): Promise<void> => {
-    // get all conversations between these two
-    const messages: Array<IChat> = await Chat.find({
+  socket.on(
+    "load conversation",
+    async ({sender, recepient}): Promise<void> => {
+      // get all conversations between these two
+      const messages: Array<IChat> = await Chat.find({
+        $or: [
+          {
+            $and: [
+              {senderEmail: sender.email},
+              {recepientEmail: recepient.email},
+            ],
+          },
+          {
+            $and: [
+              {senderEmail: recepient.email},
+              {recepientEmail: sender.email},
+            ],
+          },
+        ],
+      })
+        // .limit(500)
+        .sort({date: 1});
 
-      $or: [
-        {
-          $and: [
-            { senderEmail: sender.email },
-            { recepientEmail: recepient.email },
-          ],
-        },
-        {
-          $and: [
-            { senderEmail: recepient.email },
-            { recepientEmail: sender.email },
-          ],
-        },
-      ],
-    })
-      .limit(100)
-      .sort({ date: 1 });
+      const transformedMessages = messages.map((m) => {
+        if (m.senderEmail === sender.email) {
+          return {message: {text: m.message, id: m._id}, sender, recepient};
+        } else {
+          return {
+            message: {text: m.message, id: m._id},
+            sender: recepient,
+            recepient: sender,
+          };
+        }
+      });
 
-    const transformedMessages = messages.map((m) => {
-      if (m.senderEmail === sender.email) {
-        return { message: { text: m.message, id: m._id }, sender, recepient };
-      } else {
-        return {
-          message: { text: m.message, id: m._id },
-          sender: recepient,
-          recepient: sender,
-        };
-      }
-    });
-
-    socket.emit("loaded conversation", transformedMessages);
-  });
+      socket.emit("loaded conversation", transformedMessages);
+    }
+  );
 
   // add an event handler for when a user is blocked
   // when this happens, they should be removed from the list of whoever they blocked
   socket.on("block user", (userData, callback) => {
-    const { user, blocker } = userData
-    
+    const {user, blocker} = userData;
+
     const users = disconnectUser(socket.id);
     socket.to(user.id).emit("users on block", {users, blocker});
     callback();
